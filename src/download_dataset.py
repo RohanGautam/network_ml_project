8998a6c5b8d03651ff3c08416eee63c641c24245
Rohan Gautam <rohan1gautam@gmail.com>
2026-04-16 12:26:51 +0200

    download dataset

---
+++ src/download_dataset.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+import dotenv
+import kagglehub
+
+# for the kaggle api key
+dotenv.load_dotenv(dotenv.find_dotenv())
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+DATA_DIR = PROJECT_ROOT / "data"
+# print(DATA_DIR)
+kagglehub.competition_download("nml-2026", output_dir=str(DATA_DIR))
