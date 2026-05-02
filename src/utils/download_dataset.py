51e4f9180e081b5fe5907838b4dc59a92e008b9c
RohanGautam <rohan1gautam@gmail.com>
2026-05-02 16:46:40 +0200

    uncomment line

--- src/utils/download_dataset.py
+++ src/utils/download_dataset.py
@@ -9,4 +9,4 @@ dotenv.load_dotenv(dotenv.find_dotenv())
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 DATA_DIR = PROJECT_ROOT / "data"
 print(DATA_DIR)
-# kagglehub.competition_download("nml-2026", output_dir=str(DATA_DIR))
+kagglehub.competition_download("nml-2026", output_dir=str(DATA_DIR))
